9e00d16384e0deff1e584074f21a55fdd967e26a
Habby <matt.habermehl@vidyard.com>
2022-01-01 16:38:02 -0500

    add title and git link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <h3>Wolfram's Rule 30 in Javascript with Vue.js wrapper</h3>
+    <h4>
+      Author: Matt Habermehl, GitHub:
+      <a href="https://github.com/matthabermehl/rule-30" target="_blank"
+        >https://github.com/matthabermehl/rule-30</a
+      >
+    </h4>
     <Canvas msg="Welcome to Your Vue.js App" />
   </div>
 </template>
